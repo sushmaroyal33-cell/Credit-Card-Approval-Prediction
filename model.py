a581fa5f329118f09ce2214afae9f9acb80ba094
Sushma <sushma@gmail.com>
2026-06-28 14:56:37 +0530

    Added Univariate Analysis

--- model.py
+++ model.py
@@ -3,6 +3,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 data = pd.read_csv("dataset.csv")
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print(data["ApprovalStatus"].value_counts())
+
+sns.countplot(x="ApprovalStatus", data=data)
+plt.show()
 
 data = data.fillna(0)
 
